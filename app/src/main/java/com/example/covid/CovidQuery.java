ea96a691b0cc97bf78c8e68e1e6f4120997c56d3
wiam95 <wiam95@hotmail.com>
2020-12-05 19:31:31 -0500

    Pushing my JSON pulls to a ListView

--- app/src/main/java/com/example/covid/CovidQuery.java
+++ app/src/main/java/com/example/covid/CovidQuery.java
@@ -3,20 +3,147 @@ package com.example.covid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class CovidQuery extends AppCompatActivity {
+
+    //https://api.covid19api.com/country/CANADA/status/confirmed/live?from=2020-10-14T00:00:00Z&to=2020-10-15T00:00:00Z
+
+    //ProgressBar progressBar;
+
+    String searchQ, query1, query2, query3, query4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid_query);
 
+
+
+        //progressBar.setVisibility(View.VISIBLE);
+
+        query1 = "https://api.covid19api.com/country/";
+        query2 = "/status/confirmed/live?from=";
+        query3 = "T00:00:00Z&to=";
+        query4 = "T00:00:00Z";
+
         //Gets you from MainActivity.java
         Intent intent = getIntent();
 
-        String query = intent.getStringExtra("searchQ");
+        String countryName = intent.getStringExtra("country");
+        String startDate = intent.getStringExtra("dateStart");
+        String endDate = intent.getStringExtra("dateEnd");
+
+        searchQ = query1 + countryName + query2 + startDate + query3 + endDate + query4;
+
         //query.setText(searchQ); //Sets the passed text to eMail
 
+        //Two lines of code to get AsyncTask going to retrieve data from a site
+        CQuery req = new CQuery(); //Creates a background thread
+        req.execute(searchQ); //Type 1
+        //req.execute("https://api.covid19api.com/country/CANADA/status/confirmed/live?from=2020-10-14T00:00:00Z&to=2020-10-15T00:00:00Z");
+
     }
+
+    //Type 1, Type 2, Type 3
+    public class CQuery extends AsyncTask< String, Integer, ArrayList<String> > {
+
+        ArrayList<String> covidList = new ArrayList<String>();
+
+        protected ArrayList<String> doInBackground(String... args) {
+
+            try {
+
+                //Create URL object of what server to contact
+                URL url = new URL(args[0]);
+
+                //Open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+                //JSON --------------------
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"));
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while((line = reader.readLine()) != null) {
+
+                    sb.append(line + "\n");
+
+                }
+                String result = sb.toString(); //result is the whole string
+
+                //Convert the string to a JSON Object
+                //JSONObject jObject = new JSONObject(result);
+                  //array                             //the whole string
+                JSONArray covidData = new JSONArray(result);
+
+                //Loops through the JSON Array to obtain all the elements (in this case its JSONObjects)
+                for(int i = 0; i < covidData.length(); i++) {
+
+                    //This will get the JSON Object at that position in the array
+                    JSONObject currentObj = covidData.getJSONObject(i);
+
+                    String Cprov = currentObj.getString("Province");
+                    String Cdate = currentObj.getString("Date");
+                    int cCases = currentObj.getInt("Cases");
+
+
+                    String combined = "";
+
+                    combined += Cprov + "\n";
+                    combined += Cdate + "\n";
+                    combined += cCases;
+
+                    //Each array element is now a single string seperated by newline
+                    covidList.add(combined);
+                }
+
+
+            }catch (Exception e) { }
+
+            //province.add("InsideDoInBackground");
+            //cases.add(2);
+            return covidList;
+        }
+
+        //(Where you update your GUI)
+        //Type 2
+        public void onProgressUpdate(Integer ... args) {
+
+           // progressBar.setVisibility(View.VISIBLE);
+            //SystemClock.sleep(300);
+           // progressBar.setProgress(args[0]);
+        }
+
+        //Type 3
+        public void onPostExecute(ArrayList<String> covidList) {
+
+            //test1.setText(covidList.get(3));
+
+            //progressBar.setVisibility(View.INVISIBLE);
+
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.covid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,38 +14,91 @@ public class MainActivity extends AppCompatActivity {
     Button search;
     EditText countryName, fromDate, toDate;
 
+    public static final String ACTIVITY_NAME = "MAIN_ACTIVITY";
 
-    //https://api.covid19api.com/country/CANADA/status/confirmed/live?from=2020-10-14T00:00:00Z&to=2020-10-15T00:00:00Z
-    String searchQ, query1, query2, query3, query4;
-
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        search = findViewById(R.id.search);
-        countryName = findViewById(R.id.countryNamePrompt);
-        fromDate = findViewById(R.id.fromDatePrompt);
-        toDate = findViewById(R.id.toDatePrompt);
+        //shared preferences
+        prefs = getSharedPreferences("fileName", MODE_PRIVATE);
 
-        query1 = "https://api.covid19api.com/country/";
-        query2 = "/status/confirmed/live?from=";
-        query3 = "T00:00:00Z&to=";
-        query4 = "T00:00:00Z";
-        
-        searchQ = query1 + countryName + query2 + fromDate + query3 + toDate + query4;
+        String savedCountry = prefs.getString("CName", "Country Name");
+        String date1 = prefs.getString("Date1", "2020-01-10");
+        String date2 = prefs.getString("Date2", "2020-11-25");
+
+        search = findViewById(R.id.search);
+        countryName = (EditText) findViewById(R.id.countryNamePrompt);
+        fromDate = (EditText) findViewById(R.id.fromDatePrompt);
+        toDate = (EditText) findViewById(R.id.toDatePrompt);
+
+        countryName.setText(savedCountry);
+        fromDate.setText(date1);
+        toDate.setText(date2);
+
 
         //This creates a transition to load CovidQuery
-        Intent nextPage = new Intent(this, CovidQuery.class);
+        Intent covidPage = new Intent(this, CovidQuery.class);
 
         //When you click the search button, start the next activity
         search.setOnClickListener( click ->
         {
-            nextPage.putExtra("searchQ", searchQ);
+            String countryNamePrompt = countryName.getText().toString();
+            String fromDatePrompt = fromDate.getText().toString();
+            String toDatePrompt = toDate.getText().toString();
 
-            startActivity(nextPage);
+            covidPage.putExtra("country", countryNamePrompt);
+            covidPage.putExtra("dateStart", fromDatePrompt);
+            covidPage.putExtra("dateEnd", toDatePrompt);
+
+            startActivity(covidPage);
         });
 
     }
+
+    //Adds the string to saved prefs
+    private void saveSharedPrefs(String savedCountry, String date1, String date2) {
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("CName", savedCountry);
+        editor.putString("Date1", date1);
+        editor.putString("Date2", date2);
+
+        editor.commit();
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveSharedPrefs( countryName.getText().toString(),
+                fromDate.getText().toString(), toDate.getText().toString() );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
 }
